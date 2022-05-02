4d43c8d442b38bbf2a8ba0fe841723ceb5e02a04
Patrice Tisserand <p.tisserand@gmail.com>
2022-05-02 12:53:23 +0200

    contract.rs: format

--- src/contract.rs
+++ src/contract.rs
@@ -4,7 +4,7 @@ use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response,
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{RankResponse, InstantiateMsg, QueryMsg, NameResponse};
+use crate::msg::{InstantiateMsg, NameResponse, QueryMsg, RankResponse};
 use crate::state::{State, STATE};
 
 // version info for migration info
@@ -13,44 +13,42 @@ const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-  deps: DepsMut,
-  _env: Env,
-  info: MessageInfo,
-  msg: InstantiateMsg,
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
     // We're storing stuff in a variable called "state" of type "State"
     let state = State {
-      rank: msg.rank,
-      owner: info.sender.clone(),
-      name: msg.name,
+        rank: msg.rank,
+        owner: info.sender.clone(),
+        name: msg.name,
     };
-  
     // We're setting the contract version using a helper function we imported
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
     // We're storing state in a special variable called "STATE"
     STATE.save(deps.storage, &state)?;
-  
     // Sending a response back to the caller
     Ok(Response::new()
-      .add_attribute("method", "instantiate")
-      .add_attribute("owner", info.sender)
-      .add_attribute("rank", msg.rank.to_string()))
+        .add_attribute("method", "instantiate")
+        .add_attribute("owner", info.sender)
+        .add_attribute("rank", msg.rank.to_string()))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
-  match msg {
-      QueryMsg::GetRank {} => to_binary(&query_rank(deps)?),
-      QueryMsg::GetName {} => to_binary(&query_name(deps)?),
-  }
+    match msg {
+        QueryMsg::GetRank {} => to_binary(&query_rank(deps)?),
+        QueryMsg::GetName {} => to_binary(&query_name(deps)?),
+    }
 }
 
 fn query_rank(deps: Deps) -> StdResult<RankResponse> {
-  let state = STATE.load(deps.storage)?;
-  Ok(RankResponse { rank: state.rank })
+    let state = STATE.load(deps.storage)?;
+    Ok(RankResponse { rank: state.rank })
 }
 
 fn query_name(deps: Deps) -> StdResult<NameResponse> {
-  let state = STATE.load(deps.storage)?;
-  Ok(NameResponse { name: state.name})
+    let state = STATE.load(deps.storage)?;
+    Ok(NameResponse { name: state.name })
 }
